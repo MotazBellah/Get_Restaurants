0a341abd34814554ce317b3b25ea2784a5ef9920
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2021-01-19 22:06:34 +0200

    Display the form

--- resturants/views.py
+++ resturants/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Measurement
+from .forms import MeasurementModelForm
 from django.http import HttpResponse
 
 # Create your views here.
 def calculate_distance_view(request):
     obj = get_object_or_404(Measurement, id=1)
+    form = MeasurementModelForm(request.POST or None)
 
     context = {
         'distance': obj,
+        'form': form,
     }
 
     return render(request, 'measurements/main.html', context)
