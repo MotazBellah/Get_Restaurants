ff178b2909fb26c3aefcd91b3544d98ff87b2023
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2021-01-19 20:59:46 +0200

    Added templates and render html file

--- resturants/views.py
+++ resturants/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Measurement
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Start Project")
+def calculate_distance_view(request):
+    obj = get_object_or_404(Measurement, id=1)
+
+    context = {
+        'distance': obj,
+    }
+
+    return render(request, 'measurements/main.html', context)
