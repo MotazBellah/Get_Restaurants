cee4fe6bce70f529695455feb8d8df2817946792
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2021-01-24 21:48:17 +0200

    Insitialize the map to be empty string in the get view

--- resturants/views.py
+++ resturants/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Measurement
 from .forms import MeasurementModelForm
 from .find_resturants import findAResturant
@@ -9,47 +9,43 @@ import folium
 
 # Create your views here.
 def calculate_distance_view(request):
-    obj = get_object_or_404(Measurement, id=1)
-    form = MeasurementModelForm(request.POST or None)
 
     geolocator = Nominatim(user_agent="resturants")
     resturants_list = []
     location_ = ''
 
     map_osm = ''
-    # map_osm = folium.Map(width=800, height=500, location=[45.5236, -122.6750])
-    # folium.Marker([45.5236, -122.6750], tooltip="Click here for more", popup="TEST", icon=folium.Icon(color='purple')).add_to(map_osm)
+    if request.method == "POST":
+        form = MeasurementModelForm(request.POST)
+        if form.is_valid():
+            # form.save()
+            meal = form.cleaned_data.get('meal')
+            location_ = form.cleaned_data.get('location')
+            print(location_)
+            location = geolocator.geocode(location_, exactly_one=False)
+            for i in location:
+                print('>>>>')
+                # print(i.latitude)
+                # print(i.longitude)
+                l_lat = i.latitude
+                l_lon = i.longitude
+            point = (l_lat, l_lon)
 
-    if form.is_valid():
-        # instance = form.save(commit=False)
-        meal = form.cleaned_data.get('meal')
-        location_ = form.cleaned_data.get('location')
-        print(location_)
-        location = geolocator.geocode(location_, exactly_one=False)
-        for i in location:
-            print('>>>>')
-            # print(i.latitude)
-            # print(i.longitude)
-            l_lat = i.latitude
-            l_lon = i.longitude
-        point = (l_lat, l_lon)
+            print('//////////////////////')
+            print(point)
+            print(meal)
+            resturants_list = findAResturant(meal, point)
+            print(resturants_list)
 
-        print('//////////////////////')
-        print(point)
-        print(meal)
-        resturants_list = findAResturant(meal, point)
-        print(resturants_list)
+            map_osm = folium.Map(width='100%', height=550, location=[l_lat, l_lon], zoom_start=13)
+            folium.Marker([l_lat, l_lon], tooltip="Click here for more", popup=location_, icon=folium.Icon(color='purple')).add_to(map_osm)
+            for i in resturants_list:
+                popup_action = "<strong>" + i['name'] + "</strong><br>"
+                folium.Marker([i['lat_lng'][0], i['lat_lng'][1]], tooltip="Click here for more", popup=popup_action, icon=folium.Icon(color='blue', icon='cloud')).add_to(map_osm)
 
-        map_osm = folium.Map(width='100%', height=550, location=[l_lat, l_lon], zoom_start=13)
-        folium.Marker([l_lat, l_lon], tooltip="Click here for more", popup=location_, icon=folium.Icon(color='purple')).add_to(map_osm)
-        for i in resturants_list:
-            popup_action = "<strong>" + i['name'] + "</strong><br>"
-            folium.Marker([i['lat_lng'][0], i['lat_lng'][1]], tooltip="Click here for more", popup=popup_action, icon=folium.Icon(color='blue', icon='cloud')).add_to(map_osm)
-
-    map_osm = map_osm._repr_html_()
+        map_osm = map_osm._repr_html_()
 
     context = {
-        'distance': obj,
         'form': form,
         'map': map_osm,
         'resturants': resturants_list,
